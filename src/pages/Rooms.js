8fb73ee6b328f33d402eed137c5a1d3779c90756
karn21 <karn212000@gmail.com>
2020-04-23 14:19:07 +0530

    Initial setup

---
+++ src/pages/Rooms.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Rooms() {
+  return <div>Rooms page</div>;
+}
+
+export default Rooms;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SingleRoom() {
+  return <div>Single Room Page</div>;
+}
+
+export default SingleRoom;
