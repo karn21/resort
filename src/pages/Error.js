bc8d4e8284c1a8c604b2e8410335626b3cd44caf
karn21 <karn212000@gmail.com>
2020-07-11 14:32:59 +0530

    added hero section

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 function Error() {
-  return <Hero></Hero>;
+  return (
+    <Hero>
+      <Banner title="error 404" subtitle="page not found">
+        <Link to="/" className="btn-primary">
+          Return Home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
 
 export default Error;
